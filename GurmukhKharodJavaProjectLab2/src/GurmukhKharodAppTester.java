8aafa4be61ae06058fab962e26488cac6ff01f84
Gurmukh Kharod <gurmukhkharod@hotmail.com>
2020-05-20 21:46:30 -0700

    Commit 5 added java code-methods,etc.

--- GurmukhKharodJavaProjectLab2/src/GurmukhKharodAppTester.java
+++ GurmukhKharodJavaProjectLab2/src/GurmukhKharodAppTester.java
@@ -4,16 +4,33 @@
 
 /**
  * @author gurmu
- *
+ * Lab 2 Software Engineering
+ * Gurmukh Kharod
+ * May 20, 2020
+ * 
+ * Basic Class that has method, which takes array to get total.
  */
 public class GurmukhKharodAppTester {
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Gurmukh Kharod Message 1");
+		
+		int[] arrayGurmukh = {2,5,4,7,10,1}; //initialize array
+		int total = 0;
+		TotalArrayMethod(arrayGurmukh); //pass array
+		System.out.println("Total: " + total); //print total
+	}
+	
+	//method to add all numbers in array, values already specified
+	public static int TotalArrayMethod(int[] paramArrayGurmukh) {
+		int total = 0;
+		for(int i = 0; i < paramArrayGurmukh.length; i++) {
+			total += i;
+		}
+		return total;
 	}
 
 }
