ec9a7ca058884f68b8b088efd58e5e5c98738920
Gurmukh Kharod <gurmukhkharod@hotmail.com>
2020-05-20 21:35:04 -0700

    Commit 4 - added src file from java project

---
+++ GurmukhKharodJavaProjectLab2/src/GurmukhKharodAppTester.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author gurmu
+ *
+ */
+public class GurmukhKharodAppTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
